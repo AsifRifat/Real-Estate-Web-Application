99e247d42a4e82a9ae029906d8fe96795ee934bb
Asif Khan Rifat <asif.rifat.9@gmail.com>
2022-04-15 23:54:39 +0600

    create custom user model

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -151,5 +151,7 @@ FILE_UPLOAD_PERMISSIONS=0o640
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+AUTH_USER_MODEL = 'accounts.UserAccount'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
