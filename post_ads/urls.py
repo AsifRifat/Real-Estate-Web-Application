63108a9491f4ad3c57b65f53e0c6c336bd1d19e1
sakil7898 <71937027+sakil7898@users.noreply.github.com>
2022-04-19 22:20:16 +0600

    Delete post_ads directory

--- post_ads/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ListingsView, ListingDetailView
-
-urlpatterns = [
-    path('', ListingsView.as_view()),
-    path('<slug>', ListingDetailView.as_view()),
-]
