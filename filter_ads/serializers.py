938e956c3cc2b38b8abbeae02387a33345891a3d
sakil7898 <71937027+sakil7898@users.noreply.github.com>
2022-04-19 22:18:58 +0600

    Delete filter_ads directory

--- filter_ads/serializers.py
+++
@@ -1,3 +0,0 @@
-from rest_framework import serializers
-from post_ads.models import Listing
-from post_ads.serializers import ListingSerializer
--- filter_ads/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import FilterView
-
-urlpatterns = [
-    path('search', FilterView.as_view()),
-]
